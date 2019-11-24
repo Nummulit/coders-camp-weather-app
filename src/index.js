d36b55163490db7fedfd712ecfe2bd407e29dc12
Pawel <rylcior@gmail.com>
2019-11-24 21:34:21 +0100

    Changed city to latitude and altitude

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
 let cityName = "",
-    currentTemp = "",
-    currentWindSpeed = "",
-    currentWeatherDescription = "",
-    currentWeatherIcon = "";
+  currentTemp = "",
+  currentWindSpeed = "",
+  currentWeatherDescription = "",
+  currentWeatherIcon = "";
 
 const fiveDaysForecast = [];
 
 const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
 
-function currentWeather(cityID) {
-    const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=metric&appid=e9c5c4f3beac305dd98167aca8dcc85b`;
-    const url = corsAnywhereUrl + apiUrl;
+function currentWeather(lat, lon) {
+  const apiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=e9c5c4f3beac305dd98167aca8dcc85b`;
+  const url = corsAnywhereUrl + apiUrl;
 
-    fetch(url)
-        .then(resp => resp.json())
-        .then(resp => {
-            cityName = resp.city.name;
-            currentTemp = resp.list[0].main.temp;
-            currentWindSpeed = resp.list[0].wind.speed;
-            currentWeatherIcon = resp.list[0].weather[0].icon;
-            currentWeatherDescription = resp.list[0].weather[0].description;
-        });
+  fetch(url)
+    .then(resp => resp.json())
+    .then(resp => {
+      cityName = resp.city.name;
+      currentTemp = resp.list[0].main.temp;
+      currentWindSpeed = resp.list[0].wind.speed;
+      currentWeatherIcon = resp.list[0].weather[0].icon;
+      currentWeatherDescription = resp.list[0].weather[0].description;
+    })
+    .catch(error => console.log(`błąd: ${error}`));
 }
 
-function fiveDayThreeHourForecastData(cityID) {
-    const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=metric&appid=e9c5c4f3beac305dd98167aca8dcc85b`;
-    const url = corsAnywhereUrl + apiUrl;
+function fiveDayThreeHourForecastData(lat, lon) {
+  const apiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=e9c5c4f3beac305dd98167aca8dcc85b`;
+  const url = corsAnywhereUrl + apiUrl;
 
-    fetch(url)
-        .then(resp => resp.json())
-        .then(resp => resp.list)
-        .then(resp => {
-            resp.forEach(weather => {
-                fiveDaysForecast.push({
-                    date: weather.dt_txt,
-                    temperatura: weather.main.temp,
-                    windSpeed: weather.wind.speed,
-                    icon: weather.weather[0].icon,
-                    description: weather.weather[0].description
-                });
-            });
+  fetch(url)
+    .then(resp => resp.json())
+    .then(resp => resp.list)
+    .then(resp => {
+      resp.forEach(weather => {
+        fiveDaysForecast.push({
+          date: weather.dt_txt,
+          temperatura: weather.main.temp,
+          windSpeed: weather.wind.speed,
+          icon: weather.weather[0].icon,
+          description: weather.weather[0].description
         });
+      });
+    })
+    .catch(error => console.log(`błąd: ${error}`));
 }
