aad6a811aa6f466a58485978d630ebdd612f44cb
kj-ninja <jakubiak.krzy@gmail.com>
2019-11-25 16:51:30 +0100

    change date form to (Pon. 13:00), refactor function with three hours forecasts

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ const weatherForFewDaysTemplate = document.querySelector('#weather-for-the-day')
 const weatherForFewDaysContainer = document.querySelector('.few-days');
 
 
-
 // First letter to uppercase function
 function jsUcfirst(string) 
 {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
 
 // Clone template from html, generate current weather html and append it to
 // current weather container in html
@@ -71,28 +71,32 @@ function currentWeatherWrapper(cityName, currentTemp, currentWeatherDescription)
   currentWeatherHtml.querySelector('.temp').textContent = currentTemp;
   currentWeatherHtml.querySelector('.state-weather').textContent = jsUcfirst(currentWeatherDescription);
 
-  currenWeatherContainer.appendChild(currentWeatherHtml);
+  return currentWeatherHtml;
 }
-currentWeatherWrapper(cityName, currentTemp, currentWeatherDescription);
+currenWeatherContainer.appendChild(currentWeatherWrapper(cityName, currentTemp, currentWeatherDescription));
 
 
-
-// Clone template from html, generate weather every three hours html and append it to
-// weather every three hours container in html
-function threeHoursWeatherWrapper(fiveDaysForecast) {
-  for (let i = 0; i <= 5; i++){
-    const threeHoursWeatherHtml = document.importNode(threeHoursWeatherTemplate.content, true);
-    // TODO: Pomocnicza funkcja ktora wyciaga tylko godzine z daty!
-    threeHoursWeatherHtml.querySelector('.hour').textContent = fiveDaysForecast[i].date;
-    threeHoursWeatherHtml.querySelector('.sizeIcon').src = `http://openweathermap.org/img/wn/${fiveDaysForecast[i].icon}@2x.png`;
-    threeHoursWeatherHtml.querySelector('.temp').textContent = fiveDaysForecast[i].temperatura;
-
-    threeHoursWeatherContainer.appendChild(threeHoursWeatherHtml);
-  }
+// Clone template from html, generate weather every three hour to html 
+function forecastToHtml(forecast) {
+  const threeHoursWeatherHtml = document.importNode(threeHoursWeatherTemplate.content, true);
+  const forecastDate = new Date(forecast.date);
+  threeHoursWeatherHtml.querySelector('.hour').textContent = jsUcfirst(forecastDate.toLocaleDateString("pl",{weekday: "short"})) + " " + forecastDate.toLocaleTimeString('pl',{minute: '2-digit', hour: '2-digit'});
+  threeHoursWeatherHtml.querySelector('.sizeIcon').src = `//openweathermap.org/img/wn/${forecast.icon}@2x.png`;
+  threeHoursWeatherHtml.querySelector('.temp').textContent = forecast.temperatura;
+  return threeHoursWeatherHtml;
 }
-threeHoursWeatherWrapper(fiveDaysForecast);
+// Map method on forecasts
+function forecastsToHtml(forecasts) {
+  return forecasts.map(forecast => forecastToHtml(forecast));
+}
+// Function call on first six elements and append all forecasts to container
+forecastsToHtml(fiveDaysForecast.slice(0, 6)).forEach(html=>{
+  threeHoursWeatherContainer.appendChild(html);
+});
 
 
+
+// TODO \/\/\/\/\/ REFACTOR
 
 // Clone template from html, generate weather for the few days html and append it to
 // weather for the few days container in html
@@ -101,7 +105,7 @@ function weatherForFewDaysWrapper(fiveDaysForecast) {
     const weatherForFewDaysHtml = document.importNode(weatherForFewDaysTemplate.content, true);
     // TODO: Pomocnicza funkcja ktora wyciaga tylko godzine z daty!
     weatherForFewDaysHtml.querySelector('.day').textContent = fiveDaysForecast[i].date;
-    weatherForFewDaysHtml.querySelector('.day-icon').src = `http://openweathermap.org/img/wn/${fiveDaysForecast[i].icon}@2x.png`;
+    weatherForFewDaysHtml.querySelector('.day-icon').src = `//openweathermap.org/img/wn/${fiveDaysForecast[i].icon}@2x.png`;
     weatherForFewDaysHtml.querySelector('.day-desc').textContent = fiveDaysForecast[i].description;
     weatherForFewDaysHtml.querySelector('.day-temp').textContent = fiveDaysForecast[i].temperatura;
 
@@ -110,8 +114,10 @@ function weatherForFewDaysWrapper(fiveDaysForecast) {
 }
 weatherForFewDaysWrapper(fiveDaysForecast);
 
+//  TODO:
+//  ask about temp unit
+//  talk about styles
+//  change html class names
 
-// TODO: 
-//   change date format
-//   ask about temp unit
-//   talk about styles
+// zawartosc petli for wydzielic do osobnej funkcji (funkcja ktora mapuje pojedynczy item arraya)
+// nastepnie ta funkcje bede wywolywal w metodzie map jak w funkcji wyzej = refactor
