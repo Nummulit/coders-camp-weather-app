34f2295fadd78ffe84e83dc9ad0f826cd3e91779
Pawel <rylcior@gmail.com>
2019-11-29 18:54:22 +0100

    Changed variables to object

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-let cityName = "",
-  currentTemp = "",
-  currentWindSpeed = "",
-  currentWeatherDescription = "",
-  currentWeatherIcon = "";
+let fiveDaysForecast = [];
+const currentWeather = {
+  cityName: "",
+  temperature: "",
+  windSpeed: "",
+  description: "",
+  icon: ""
+};
 const currentDayForecast = [];
-const fiveDaysForecast = [];
-
 const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
 
 function fiveDayThreeHourForecastData(lat, lon) {
   const apiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=e9c5c4f3beac305dd98167aca8dcc85b`;
 
   const url = corsAnywhereUrl + apiUrl;
+
   fetch(url)
     .then(resp => resp.json())
     .then(resp => {
-      cityName = resp.city.name;
-      currentTemp = Math.floor(resp.list[0].main.temp);
-      currentWindSpeed = resp.list[0].wind.speed;
-      currentWeatherIcon = resp.list[0].weather[0].icon;
-      currentWeatherDescription = resp.list[0].weather[0].description;
+      console.log(resp);
+      currentWeather.cityName = resp.city.name;
+      currentWeather.temperature = Math.floor(resp.list[0].main.temp);
+      currentWeather.windSpeed = resp.list[0].wind.speed;
+      currentWeather.icon = resp.list[0].weather[0].icon;
+      currentWeather.description = resp.list[0].weather[0].description;
 
       for (const weather of resp.list) {
         if (weather.dt_txt[12] == 0) {
@@ -29,24 +32,27 @@ function fiveDayThreeHourForecastData(lat, lon) {
             date: weather.dt_txt,
             temperatura: Math.floor(weather.main.temp),
             windSpeed: weather.wind.speed,
-            description: weather.weather[0].description
+            description: weather.weather[0].description,
+            icon: weather.weather[0].icon
           });
         }
       }
+      fiveDaysForecast = [];
       resp.list.forEach(weather => {
         if (weather.dt_txt[11] == 1 && weather.dt_txt[12] == 2) {
           fiveDaysForecast.push({
             date: weather.dt_txt,
             temperatura: Math.floor(weather.main.temp),
             windSpeed: weather.wind.speed,
-            description: weather.weather[0].description
+            description: weather.weather[0].description,
+            icon: weather.weather[0].icon
           });
         }
       });
     })
     .catch(error => {
       console.log(`błąd: ${error}`);
-      window.alert("Błąd z połączeniem, spróbuj ponownie za kilka minut");
+      window.alert("Error, please try again later");
     });
 }
 
